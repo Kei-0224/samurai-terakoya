1b1cdc2bc5a4df5ac099441e31a7009b5abe8b74
Kei-0224 <mugenndai@icloud.com>
2025-06-08 08:12:53 +0900

    Add files via upload

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,8 +1,12 @@
-// ① 画面（DOM）から要素を一度だけ取得して定数に入れる
-const btn  = document.getElementById("btn");  
-const text = document.getElementById("text");  
 
-// ② 取得した定数を使ってイベントを登録
-btn.addEventListener("click", () => {
-  text.textContent = "ボタンをクリックしました";
-});
+ // ① 画面（DOM）から要素を一度だけ取得して定数に入れる
+    const btn  = document.getElementById("btn");  
+    const text = document.getElementById("text");
+
+    // ② ボタンがクリックされたら処理を実行
+    btn.addEventListener("click", () => {
+      // ③ 2秒（2000ミリ秒）の待ち時間を設定し、非同期処理を実行する
+      setTimeout(() => {
+        text.textContent = "ボタンをクリックしました";
+      }, 2000);
+         });
