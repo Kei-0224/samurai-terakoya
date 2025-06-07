c5c92041f000013319e3a09c71ddd222d8a2a7b7
Kei-0224 <mugenndai@icloud.com>
2025-06-07 14:06:35 +0900

    Add files via upload

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,3 +1,8 @@
-document.getElementById("btn").addEventListener("click", function() {
-  document.getElementById("text").textContent = "ボタンをクリックしました";
+// ① 画面（DOM）から要素を一度だけ取得して定数に入れる
+const btn  = document.getElementById("btn");  
+const text = document.getElementById("text");  
+
+// ② 取得した定数を使ってイベントを登録
+btn.addEventListener("click", () => {
+  text.textContent = "ボタンをクリックしました";
 });
